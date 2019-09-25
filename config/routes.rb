c066752a00a222d81cd6dc2e251d19475524f3a3
michael h <michael.hamouda1@gmail.com>
2019-09-25 21:16:41 +0000

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/" => "statics#home", as: :home
+  get "/x" => "statics#home", as: :home
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
