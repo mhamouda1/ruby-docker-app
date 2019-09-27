86f92be6e7e118f2d32e45a303660cc7d39aefe1
michael h <michael.hamouda1@gmail.com>
2019-09-27 20:36:26 +0000

    wip

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,8 +2,10 @@
 require 'action_dispatch/middleware/session/dalli_store'
 
 # Rails.application.config.session_store :cookie_store, key: '_ruby-docker-app_session' # default
-if Rails.env.development?
-  Rails.application.config.session_store :dalli_store, :memcache_server => ['memcached'], :namespace => 'sessions', :key => '_foundation_session', :expire_after => 5.days
-elsif Rails.env.production?
-  Rails.application.config.session_store :dalli_store, :memcache_server => ['my-memcached.mfh9as.0001.use1.cache.amazonaws.com'], :namespace => 'sessions', :key => '_foundation_session', :expire_after => 5.days
-end
+# if Rails.env.development?
+  # Rails.application.config.session_store :dalli_store, :memcache_server => ['memcached'], :namespace => 'sessions', :key => '_foundation_session', :expire_after => 5.days
+# elsif Rails.env.production?
+  # Rails.application.config.session_store :dalli_store, :memcache_server => ['my-memcached.mfh9as.0001.use1.cache.amazonaws.com'], :namespace => 'sessions', :key => '_foundation_session', :expire_after => 5.days
+# end
+
+Rails.application.config.session_store :dalli_store, :memcache_server => [ENV["MEMCACHED_SERVER"]], :namespace => 'sessions', :key => '_foundation_session', :expire_after => 5.days
