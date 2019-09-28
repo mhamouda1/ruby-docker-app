021704d2d997622778b58196bd77e12f97bb274d
michael h <michael.hamouda1@gmail.com>
2019-09-28 03:05:46 +0000

    wip

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -3,18 +3,16 @@ cd /home/ec2-user/ruby-docker-app
 sudo bash -c "echo 'NODE_HOST=$(curl http://169.254.169.254/latest/meta-data/local-hostname)' >> .env"
 
 sudo docker-compose down
-sudo docker-compose kill
 
 sudo git pull
 sudo git stash #gemfile.lock error if don't git stash
 sudo git pull
 
-sudo docker-compose build
-sudo docker-compose up -d
-sleep 7
-sudo docker-compose run web bundle update
-sudo docker-compose run web rake db:create
-sudo docker-compose run web rake db:migrate
-sudo docker-compose run web rake assets:precompile
+#sudo docker-compose build
+#sudo docker-compose run -d web
+#sudo docker-compose run web bundle update
+sudo docker-compose run -p 5000:3000 web rake db:create
+sudo docker-compose run -p 5000:3000 web rake db:migrate
+sudo docker-compose run -p 5000:3000 web rake assets:precompile
 sudo docker-compose down
 sudo docker-compose up -d
