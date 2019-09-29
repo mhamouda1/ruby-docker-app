0d0f8a3838b25f6f8d4e113c396c7cece76ac63d
michael h <michael.hamouda1@gmail.com>
2019-09-29 18:26:27 +0000

    wip

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,9 @@ resource "aws_key_pair" "my_key_pair" {
   key_name   = "${terraform.workspace}_my_key_pair"
   public_key = file("/root/.ssh/id_rsa.pub")
 }
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
