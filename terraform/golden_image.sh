58f20d3cefaff437af867d8b4049ca854012d121
michael h <michael.hamouda1@gmail.com>
2019-09-29 01:38:19 +0000

    done for today

--- terraform/golden_image.sh
+++ terraform/golden_image.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 chmod 400 default_my_key_pair.pem
+
+#install docker
 sudo yum install docker -y
 sudo yum install git -y
 sudo curl -L 'https://github.com/docker/compose/releases/download/1.24.1/docker-compose-Linux-x86_64' -o /usr/local/bin/docker-compose
@@ -8,6 +10,7 @@ sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo service docker start
 sudo systemctl enable docker
 
+#download the code
 cd /home/ec2-user
 sudo git clone https://github.com/mhamouda1/ruby-docker-app
 cd /home/ec2-user/ruby-docker-app
@@ -18,6 +21,7 @@ array="${@}"
 echo $array | tr " " "\n" >> .env
 sudo bash -c "echo 'RAILS_SERVE_STATIC_FILES=true' >> .env"
 
+#run db migrations and compile assets
 sudo $(aws ecr get-login --no-include-email --region us-east-1)
 sudo docker-compose pull
 sudo docker-compose run -p 5000:3000 web bash -c "bundle install && rake db:create && rake db:migrate && rake assets:precompile"
--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/ruby-docker-app
 
+#export env variables
 sudo bash -c "echo 'NODE_HOST=$(curl http://169.254.169.254/latest/meta-data/local-hostname)' >> .env"
 export $(cat .env | xargs)
 
@@ -10,6 +11,7 @@ sudo git pull
 sudo git stash #gemfile.lock error if don't git stash
 sudo git pull
 
+#run migrations, compile assets, and run server
 sudo docker-compose run -p 5000:3000 web bash -c "bundle install && rake db:create && rake db:migrate && rake assets:precompile"
 sudo aws s3 cp public/assets/ s3://$S3_BUCKET/assets/ --recursive --include "*" --acl "public-read"
 sudo docker-compose down
