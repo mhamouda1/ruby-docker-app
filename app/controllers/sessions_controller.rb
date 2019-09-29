8704d88d7e5a10202cb0941018ece96b294d0151
michael h <michael.hamouda1@gmail.com>
2019-09-29 03:25:35 +0000

    wip

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       redirect_to home_path, notice: "Logged in!"
     else
       flash.now.alert = "Email or password is invalid"
-      render "new"
+      render :new
     end
   end
 
