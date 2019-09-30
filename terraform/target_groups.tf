837528a3411786444dfebc38187baaf77f058b99
michael h <michael.hamouda1@gmail.com>
2019-09-30 23:15:41 +0000

    wip

--- terraform/target_groups.tf
+++ terraform/target_groups.tf
@@ -1,17 +1,23 @@
-# resource "aws_lb_target_group" "target_group_1" {
-  # name     = "my-target-group-1"
-  # port     = 3000
-  # protocol = "HTTP"
-  # vpc_id   = "${aws_vpc.main.id}"
-  # deregistration_delay = 0
+resource "aws_lb_target_group" "target_group_1" {
+  name     = "my-target-group-1"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = "${aws_vpc.main.id}"
+  deregistration_delay = 0
 
-  # health_check {
-    # healthy_threshold = 5
-    # unhealthy_threshold = 2
-    # timeout = 5
-    # interval = 6
-  # }
-# }
+  health_check {
+    healthy_threshold = 5
+    unhealthy_threshold = 2
+    timeout = 5
+    interval = 6
+  }
+}
+
+resource "aws_lb_target_group_attachment" "instance_1" {
+  target_group_arn = "${aws_lb_target_group.target_group_1.arn}"
+  target_id        = "${aws_instance.master.id}"
+  port             = 3000
+}
 
 #resource "aws_lb_target_group" "target_group_2" {
 #  name     = "my-target-group-2"
@@ -20,12 +26,6 @@
 #  vpc_id   = "${aws_vpc.main.id}"
 #}
 
-#resource "aws_lb_target_group_attachment" "instance_1" {
-#  target_group_arn = "${aws_lb_target_group.target_group_1.arn}"
-#  target_id        = "${aws_instance.web_public_1.id}"
-#  port             = 3000
-#}
-#
 #resource "aws_lb_target_group_attachment" "instance_2" {
 #  target_group_arn = "${aws_lb_target_group.target_group_1.arn}"
 #  target_id        = "${aws_instance.web_public_2.id}"
