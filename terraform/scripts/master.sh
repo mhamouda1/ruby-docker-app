f41e48d5293df7c04ce32f8913ea41384bf89e25
michael h <michael.hamouda1@gmail.com>
2019-10-01 16:18:04 +0000

    wip

--- terraform/scripts/master.sh
+++ terraform/scripts/master.sh
@@ -3,6 +3,16 @@ chmod 400 default_my_key_pair.pem
 chmod 400 /home/ec2-user/.ssh/id_rsa
 export $1
 
+#add some swap memory
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576 && \
+sudo chmod 600 /swapfile && \
+sudo mkswap /swapfile && \
+echo '/swapfile            swap swap    0   0' >> /etc/fstab && \
+sudo mount -a && \
+sudo swapon -s && \
+sudo swapon /swapfile && \
+free -h
+
 #docker pre-reqs
 sudo yum update -y
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
