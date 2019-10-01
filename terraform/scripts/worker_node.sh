cb1251ea768c3cb36f831ed8cabae22edfa6ff1f
michael h <michael.hamouda1@gmail.com>
2019-10-01 23:54:46 +0000

    swap memory worker

--- terraform/scripts/worker_node.sh
+++ terraform/scripts/worker_node.sh
@@ -47,3 +47,13 @@ sudo bash -c "echo \"127.0.0.1 $(hostname)\" >> /etc/hosts"
 hostnamectl set-hostname $NODE-$HOSTNAME
 sudo aws s3 cp s3://$S3_BUCKET/kubernetes_join.txt kubernetes_join.txt
 sudo $(cat kubernetes_join.txt)
+
+#add swap memory
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576 && \
+sudo chmod 600 /swapfile && \
+sudo mkswap /swapfile && \
+echo '/swapfile            swap swap    0   0' >> /etc/fstab && \
+sudo mount -a && \
+sudo swapon -s && \
+sudo swapon /swapfile && \
+free -h
